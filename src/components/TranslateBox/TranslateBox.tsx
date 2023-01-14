9b9dd651dbdea08c0c8808bfa0844fd9f2d558b2
Slavo Seman <slavosmn@gmail.com>
2023-01-14 10:22:13 +0100

    Update TranslateBox and TranslateWord with styling

--- src/components/TranslateBox/TranslateBox.tsx
+++ src/components/TranslateBox/TranslateBox.tsx
@@ -1,69 +1,42 @@
-import { PageContainer } from '@ant-design/pro-layout';
 import { Switch } from 'antd';
 import { useState, useEffect } from 'react';
-import { Tooltip } from "react-tippy";
 
-interface TranslateWordProps {
-  word: string;
-  translation: string;
-}
-
-const TranslateWord: React.FC<TranslateWordProps> = ({ word, translation }) => {
-    const handleClick = useCallback(async () => {
-      // Perform POST request here. For example, using the fetch API:
-      try {
-        const response = await fetch(`https://your-api-endpoint.com/translate`, {
-          method: 'POST',
-          body: JSON.stringify({ word }),
-          headers: { 'Content-Type': 'application/json' },
-        });
-        // Handle the response
-      } catch (error) {
-        console.error(error);
-      }
-    }, [word]);
-  
-    return (
-      <Tooltip title={translation} trigger="mouseenter">
-        <span onClick={handleClick}>{word} </span>
-      </Tooltip>
-    );
-  };
+import TranslateWord from '../TranslateWord/TranslateWord';
 
 interface TranslateBoxProps {
   text: string;
-  translations: string[];
 }
 
 const TranslateBox: React.FC<TranslateBoxProps> = ({text}) => {
-    const [translations, setTranslations] = useState([]);
+    const [translations, setTranslations] = useState<string[]>([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const [mode, setMode] = useState('word'); // initial mode is 'word'
+    const [error, setError] = useState<Error | null>(null);
+    const [mode, setMode] = useState<'word' | 'sentence'>('word'); // initial mode is 'word'
+    const [clickedWords, setClickedWords] = useState<string[]>([]);
   
-    const dummyFetch = async (text, mode) => {
-        // This is an example of how the data might be structured in a real API response
-        return {
-            translations: mode === 'word' ? 
-                text.split(" ").map((word) => `${word}_translated`) : 
-                [`${text}_translated`]
-        };
-    };
+    const handleWordClick = (word: string) => {
+      if(!clickedWords.includes(word)){
+          setClickedWords([...clickedWords, word]);
+      }
+  }
 
-    // useEffect can stay the same 
-    useEffect(() => {
+  const dummyFetch = async (text: string, mode: 'word' | 'sentence') => {
+    return {
+        translations: mode === 'word' ? 
+            text.split(" ").map((word) => `${word}_translated`) : 
+            [`${text}_translated`]
+    };
+};
+useEffect(() => {
       const fetchData = async () => {
         setIsLoading(true);
         try {
           let data
           if(mode === "word"){
-            //response = await fetch(`https://your-api-endpoint.com/translate?text=${text}&mode=word`);
             data = await dummyFetch(text,'word');
         }else{
-            //response = await fetch(`https://your-api-endpoint.com/translate?text=${text}&mode=sentence`);
             data = await dummyFetch(text,'sentence');
         }
-          //const data = await response.json();
           setTranslations(data.translations);
         } catch (err) {
           setError(err);
@@ -82,7 +55,7 @@ const TranslateBox: React.FC<TranslateBoxProps> = ({text}) => {
       return <div>An error occurred: {error.message}</div>;
     }
   
-    const words = mode === 'word' ? text.split(" ") : [text]; // split text by " " for translating words mode
+    const words = mode === 'word' ? text.split(" ") : [text]; 
     return (
       <>
             <div style={{ marginBottom: 16 }}>
@@ -96,11 +69,19 @@ const TranslateBox: React.FC<TranslateBoxProps> = ({text}) => {
             </div>
             <div className="translate-box">
                 {words.map((word, index) => (
-                    <TranslateWord key={word} word={word} translation={translations[index]} />
+                    <TranslateWord key={word} word={word} translation={translations[index]} onClick={handleWordClick} />
                 ))}
             </div>
+            <div className="clicked-words-list">
+            Clicked Words:
+            <ul>
+                {clickedWords.map((word, index) => (
+                    <li key={index}>{word}</li>
+                ))}
+            </ul>
+        </div>
 </>
     );
   };
 
-export default TranslateBox;
+export default TranslateBox
--- src/pages/book/index.tsx
+++ src/pages/book/index.tsx
@@ -1,15 +1,35 @@
 import TranslateBox from "@/components/TranslateBox/TranslateBox";
 import { PageContainer } from "@ant-design/pro-layout";
-import { Card } from "antd";
+import { Card, Row, Col } from "antd";
 
 export default function Book() {
+    const [clickedWords, setClickedWords] = useState<string[]>([]); // added state to keep track of clicked words
     const text = "This is some text to be translated";
-  const translations = ["Translation 1","Translation 2","Translation 3","Translation 4","Translation 5","Translation 6","Translation 7"];
+    const translations = ["Translation 1","Translation 2","Translation 3","Translation 4","Translation 5","Translation 6","Translation 7"];
+
+    const handleClick = (word: string) => {
+        setClickedWords([...clickedWords, word]);
+    }
+
     return (
         <PageContainer>
-            <Card>
-                <TranslateBox text={text} translations={translations}></TranslateBox>
-            </Card>
+            <Row>
+                <Col span={12}>
+                    <Card>
+                        <TranslateBox text={text} translations={translations} onClick={handleClick} />
+                    </Card>
+                </Col>
+                <Col span={12}>
+                    <Card title="Translate List">
+                        <div>Clicked Words:</div>
+                        <ul>
+                            {clickedWords.map((word, index) => (
+                                <li key={index}>{word}</li>
+                            ))}
+                        </ul>
+                    </Card>
+                </Col>
+            </Row>
         </PageContainer>
-            )
+    )
 }
