575fa1b01ed0e2ecb75608b52364f135989ee177
Slavo Seman <slavosmn@gmail.com>
2023-01-16 21:36:24 +0100

    Modify rollup option, may cause problem with amplify

--- vite.config.ts
+++ vite.config.ts
@@ -85,9 +85,6 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          'react-venders': ['react', 'react-dom', '@vitjs/runtime'],
-        },
       },
     },
   },
